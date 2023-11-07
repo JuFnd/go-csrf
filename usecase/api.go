37704ea1cca3ef96a84e596857169efd3535c529
Islam <109366718+JuFnd@users.noreply.github.com>
2023-11-08 00:56:48 +0300

    Update api.go

--- usecase/api.go
+++ usecase/api.go
@@ -49,6 +49,32 @@ func (a *API) ListenAndServe() {
 	}
 }
 
+func (a *API) GetCsrfToken(w http.ResponseWriter, r *http.Request) {
+	response := Response{Status: http.StatusOK, Body: nil}
+
+	csrfToken := r.Header.Get("x-csrf-token")
+
+	found, err := a.core.CheckCsrfToken(csrfToken)
+	if csrfToken != "" && found {
+		w.Header().Set("X-CSRF-Token", csrfToken)
+		a.SendResponse(w, response)
+		return
+	}
+
+	token, err := a.core.CreateCsrfToken()
+
+	if err != nil {
+		w.Header().Set("X-CSRF-Token", "null")
+		response.Status = 502
+		a.SendResponse(w, response)
+	}
+
+	w.Header().Set("X-CSRF-Token", token)
+	a.SendResponse(w, response)
+	return
+
+}
+
 func (a *API) SendResponse(w http.ResponseWriter, response Response) {
 	jsonResponse, err := json.Marshal(response)
 	if err != nil {
